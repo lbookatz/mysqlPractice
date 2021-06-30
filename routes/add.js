0fa664d28c39e203f6cd12df075df81f48d21f4b
Your Name <lbookatz@hotmail.com>
2021-06-30 10:13:11 +0100

    added get to add

--- routes/add.js
+++ routes/add.js
@@ -4,7 +4,12 @@ const Movies = require("../models/movies")
 const Users = require("../models/Users")
 const Favorites = require("../models/Favorites")
 
-router.get ()
+router.get ('/', async (req,res) => {
+    let users = "<h1>Users</h1><br>"
+    // users = users + await json(Users.findAll({}));
+    res.status(200).json(await Users.findAll({}));
+    ;
+})
 
 router.post('/',async (req,res) => {
     await Movies.create({name:req.body.movie});
